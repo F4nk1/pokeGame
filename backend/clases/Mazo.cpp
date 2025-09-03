a4e1d1a9432d64cd8065ca3cbb639ca4481796c0
jose daniel mendoza <bydenveryt@gmail.com>
2025-09-03 09:57:10 -0500

    estructuracion del proyecto

--- backend/clases/Mazo.cpp
+++ backend/clases/Mazo.cpp
@@ -14,18 +14,25 @@ void Mazo::setCartas(const std::vector<Carta>& cartas) {
 }
 
 // Otros métodos
+
+// revisa si el mazo esta vacio
 bool Mazo::EstaVacio() const {
     return cartas.empty();
 }
 
+// agrega una carta al mazo
 void Mazo::AgregarCarta(const Carta& carta) {
     cartas.push_back(carta);
 }
+
+// elimina una carta del mazo por su id
 void Mazo::EliminarCarta(int id) {
     cartas.erase(std::remove_if(cartas.begin(), cartas.end(),
                                 [id](const Carta& carta) { return carta.getId() == id; }),
                  cartas.end());
 }
+
+// baraja las cartas del mazo
 void Mazo::Barajar() {
     std::random_device rd;
     std::mt19937 g(rd());
